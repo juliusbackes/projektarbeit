a5015e9f1882a5880b2f3439a12cace0ea888c53
juliusbackes <115353924+juliusbackes@users.noreply.github.com>
2024-09-21 20:19:15 +0200

    title page app link

--- web/projektarbeit/src/routes/+page.svelte
+++ web/projektarbeit/src/routes/+page.svelte
@@ -4,5 +4,6 @@
 
 </script>
 
-
-Title tom
+<div class="flex justify-center h-screen items-center ">
+    <a href="/app" class="bg-emerald-700 px-3 py-2 flex items-center justify-center max-w-16 rounded-lg text-white">App</a>
+</div>
