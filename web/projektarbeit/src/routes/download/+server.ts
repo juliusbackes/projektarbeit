3ca46932dcda12f8a1d096ff4d55e78f1a8bee70
juliusbackes <115353924+juliusbackes@users.noreply.github.com>
2024-12-15 21:43:05 +0100

    fix

--- web/projektarbeit/src/routes/download/+server.ts
+++ web/projektarbeit/src/routes/download/+server.ts
@@ -6,12 +6,12 @@ import { RESEND_API_KEY } from '$env/static/private';
 const resend = new Resend(RESEND_API_KEY);
 
 export const GET = async () => {
-    const filePath = path.resolve('static/Projektarbeit.zip');
+    const filePath = path.resolve('./Projektarbeit.zip');
     const fileBuffer = await fs.promises.readFile(filePath);
 
     await resend.emails.send({
         from: 'Projektarbeit <team@pa.juliusbackes.com>',
-        to: ['tomkurzke@gmail.com', 'juliusbackes@icloud.com'],
+        to: ['juliusbackes@icloud.com'],
         subject: 'Projektarbeit Download Notification',
         html: `
             <p>Jemand hat die Projektarbeit heruntergeladen.</p>
