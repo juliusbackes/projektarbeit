e76530f54895013765b5c279616977410f49b56a
juliusbackes <115353924+juliusbackes@users.noreply.github.com>
2025-02-10 09:16:03 +0100

    fix: user dropdown sidebar crashing

--- web/projektarbeit/src/lib/components/Sidebar/SidebarUser.svelte
+++ web/projektarbeit/src/lib/components/Sidebar/SidebarUser.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
-	import * as Avatar from "$lib/components/ui/avatar/index.js";
-	import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-	import { useSidebar } from "$lib/components/ui/sidebar/index.js";
-    import { LogOut, ChevronsUpDown, BadgeCheck } from "lucide-svelte";
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	import { LogOut, ChevronsUpDown, BadgeCheck } from 'lucide-svelte';
 
 	let { user }: any = $props();
 
-    let logoutForm: HTMLFormElement;
+	let logoutForm: HTMLFormElement;
 
 	const sidebar = useSidebar();
+
+	const name =
+		user.user_metadata.display_name ?? user.user_metadata.name ?? user.email.split('@')[0];
+	const initials = name
+		.split(' ')
+		.map((n: string) => n[0])
+		.join('');
 </script>
 
 <Sidebar.Menu>
@@ -23,10 +30,12 @@
 						{...props}
 					>
 						<Avatar.Root class="h-8 w-8 rounded-lg">
-							<Avatar.Fallback class="rounded-lg">{user.user_metadata.display_name ? user.user_metadata.display_name.split(" ").map((name: string) => name[0]).join("") : user.email.split(".")[0]}</Avatar.Fallback>
+							<Avatar.Fallback class="rounded-lg">
+								{initials.toUpperCase()}
+							</Avatar.Fallback>
 						</Avatar.Root>
 						<div class="grid flex-1 text-left text-sm leading-tight">
-							<span class="truncate font-semibold">{user.user_metadata.display_name ?? user.email}</span>
+							<span class="truncate font-semibold">{name}</span>
 							<span class="truncate text-xs">{user.email}</span>
 						</div>
 						<ChevronsUpDown class="ml-auto size-4" />
@@ -35,37 +44,39 @@
 			</DropdownMenu.Trigger>
 			<DropdownMenu.Content
 				class="w-[--bits-dropdown-menu-anchor-width] min-w-56 rounded-lg"
-				side={sidebar.isMobile ? "bottom" : "right"}
+				side={sidebar.isMobile ? 'bottom' : 'right'}
 				align="end"
 				sideOffset={4}
 			>
 				<DropdownMenu.Label class="p-0 font-normal">
 					<div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 						<Avatar.Root class="h-8 w-8 rounded-lg">
-                            <Avatar.Fallback class="rounded-lg">{user.user_metadata.display_name.split(" ").map((name: string) => name[0]).join("")}</Avatar.Fallback>
+							<Avatar.Fallback class="rounded-lg">
+								{initials.toUpperCase()}
+							</Avatar.Fallback>
 						</Avatar.Root>
 						<div class="grid flex-1 text-left text-sm leading-tight">
-							<span class="truncate font-semibold">{user.user_metadata.display_name ?? user.email}</span>
+							<span class="truncate font-semibold">{name}</span>
 							<span class="truncate text-xs">{user.email}</span>
 						</div>
 					</div>
 				</DropdownMenu.Label>
 				<DropdownMenu.Separator />
 				<DropdownMenu.Group>
-                    <a href="/app/account">
-                        <DropdownMenu.Item>
-                            <BadgeCheck />
-                            Konto
-                        </DropdownMenu.Item>
-                    </a>
+					<a href="/app/account">
+						<DropdownMenu.Item>
+							<BadgeCheck />
+							Konto
+						</DropdownMenu.Item>
+					</a>
 				</DropdownMenu.Group>
 				<DropdownMenu.Separator />
-                <form method="post" action="/logout" bind:this={logoutForm}>
-                    <DropdownMenu.Item onclick={() => logoutForm.submit()}>
-                        <LogOut />
-                        Abmelden
-                    </DropdownMenu.Item>
-                </form>
+				<form method="post" action="/logout" bind:this={logoutForm}>
+					<DropdownMenu.Item onclick={() => logoutForm.submit()}>
+						<LogOut />
+						Abmelden
+					</DropdownMenu.Item>
+				</form>
 			</DropdownMenu.Content>
 		</DropdownMenu.Root>
 	</Sidebar.MenuItem>
